7ccd5dfcf42623595c18e9c9a66aabc172f2de47
DimaDK <DimaKrutDK@ya.ru>
2018-12-15 15:51:58 +0300

    Updates

---
+++ scripts/a.js
@@ -0,0 +1,23 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["a"],{
+
+/***/ "./src/a.js":
+/*!******************!*\
+  !*** ./src/a.js ***!
+  \******************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+function a() {
+    console.log('a is called')
+}
+
+console.log('a is imported')
+/* harmony default export */ __webpack_exports__["default"] = (a);
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=a.js.map
